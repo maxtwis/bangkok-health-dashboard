8c8ab5e3e36cc37d4bf6261cbdfc9b19c8e2e06b
maxtwis <napat.trs@gmail.com>
2025-07-05 21:57:16 +0700

    vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts'],
+          maps: ['leaflet'],
+          utils: ['papaparse', 'lodash']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['leaflet', 'papaparse', 'lodash', 'recharts']
+  }
 })
