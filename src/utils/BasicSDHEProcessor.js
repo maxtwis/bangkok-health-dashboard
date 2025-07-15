e3ab50b61f4f9dbc84cbe56634d479bc5b8b0a88
maxtwis <napat.trs@gmail.com>
2025-07-15 15:17:17 +0700

    Update BasicSDHEProcessor.js

--- src/utils/BasicSDHEProcessor.js
+++ src/utils/BasicSDHEProcessor.js
@@ -168,7 +168,7 @@ class BasicSDHEProcessor {
       healthcare_access: {
         health_coverage: { 
           field: 'welfare', 
-          condition: (val) => val !== null,
+          condition: (val) => val === 1 || val === 2 || val === 3,
           label: 'Health Coverage'
         },
         medical_consultation_skip_cost: { 
