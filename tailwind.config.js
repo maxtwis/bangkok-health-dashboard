58bc8292111a8b6e537b4b173254b50631101d35
maxtwis <napat.trs@gmail.com>
2025-08-24 16:45:26 +0700

    font changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Mitr', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+        'mitr': ['Mitr', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
